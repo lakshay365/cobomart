3e1c1067ebbb782d617c87cf1216fbce6a5c2752
Manish Devgan <manish.nsit8@gmail.com>
2018-04-08 11:06:29 +0530

    Adds Submit Functionality

--- routes/ad.js
+++ routes/ad.js
@@ -1,3 +1,5 @@
+const Ad = require('../models/ad')
+
 function loggedIn(req, res, next) {
   if (!req.user) res.redirect('/login')
   else next()
@@ -9,6 +11,22 @@ module.exports = app => {
   })
 
   app.post('/submit', loggedIn, (req, res) => {
-    // TODO
+    var book = new Ad({
+    	title: req.body.title,
+    	description: req.body.description,
+    	price: req.body.price,
+    	negotiable: (req.body.negotiable == 'on')?true:false,
+    	condition: req.body.condition,
+    	user: req.user._id
+    })
+
+    book.save(function(err){
+    	if(err){
+    		return console.log(err)
+    	}
+    	console.log('saved')
+    })
+
+    res.render('submit')
   })
 }
