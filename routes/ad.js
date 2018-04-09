7aafd38663020c61c62d2ae35d79caf4395c6f5e
Lakshay Mehra <lakshay344@gmail.com>
2018-04-09 21:13:14 +0530

    Add functionality to edit advertisements

--- routes/ad.js
+++ routes/ad.js
@@ -30,4 +30,35 @@ module.exports = app => {
       .then(ad => res.redirect(`/marketplace/${ad._id}`))
       .catch(err => console.log(err))
   })
+
+  app.get('/edit/:id', loggedIn, (req, res) => {
+    Ad.findById(req.params.id)
+      .populate('user')
+      .exec()
+      .then(ad => {
+        if (!req.user._id.equals(ad.user._id)) {
+          return Promise.reject('Not allowed')
+        }
+
+        res.render('submit', {
+          title: `Edit - ${ad.title}`,
+          ad: ad,
+          user: req.user
+        })
+      })
+      .catch(err => res.redirect('/'))
+  })
+
+  app.post('/edit', loggedIn, (req, res) => {
+    Ad.findByIdAndUpdate(req.body.id, {
+      title: req.body.title || 'Untitled',
+      description: req.body.description,
+      price: req.body.price || 0,
+      negotiable: req.body.negotiable === 'on',
+      condition: req.body.condition,
+      user: req.user._id
+    })
+      .then(ad => res.redirect(`/edit/${ad.id}`))
+      .catch(err => console.log(err))
+  })
 }
